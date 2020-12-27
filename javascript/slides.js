6fb09a5ed4b9f30c3b4bb1781125649432bdc9c3
Miles T Stein <milltir@gmail.com>
2020-12-27 16:19:25 -0500

    jfc

--- javascript/slides.js
+++ javascript/slides.js
@@ -1,3 +1,24 @@
+var slideIndex = 1;
+
+var myTimer;
+
+var slideshowContainer;
+
+window.addEventListener("load",function() {
+    showSlides(slideIndex);
+    myTimer = setInterval(function(){plusSlides(1)}, 4000);
+  
+    //COMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+    slideshowContainer = document.getElementsByClassName('slideshow-inner')[0];
+  
+    //UNCOMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+    // slideshowContainer = document.getElementsByClassName('slideshow-container')[0];
+  
+    slideshowContainer.addEventListener('mouseenter', pause)
+    slideshowContainer.addEventListener('mouseleave', resume)
+})
+
+// NEXT AND PREVIOUS CONTROL
 function plusSlides(n){
   clearInterval(myTimer);
   if (n < 0){
@@ -5,11 +26,21 @@ function plusSlides(n){
   } else {
    showSlides(slideIndex += 1); 
   }
+  
+  //COMMENT OUT THE LINES BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+  
   if (n === -1){
     myTimer = setInterval(function(){plusSlides(n + 2)}, 4000);
   } else {
     myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
   }
+}
+
+//Controls the current slide and resets interval if needed
+function currentSlide(n){
+  clearInterval(myTimer);
+  myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
+  showSlides(slideIndex = n);
 }
 
 function showSlides(n){
@@ -28,14 +59,11 @@ function showSlides(n){
   dots[slideIndex-1].className += " active";
 }
 
-window.addEventListener("load",function() {
-    showSlides(slideIndex);
-    myTimer = setInterval(function(){plusSlides(1)}, 4000);
-})
-
-function currentSlide(n){
+pause = () => {
   clearInterval(myTimer);
-  myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
-  showSlides(slideIndex = n);
 }
 
+resume = () =>{
+  clearInterval(myTimer);
+  myTimer = setInterval(function(){plusSlides(slideIndex)}, 4000);
+}
