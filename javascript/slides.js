6aed86f9a19d741376088238219ded7e1e083f86
Miles T Stein <milltir@gmail.com>
2020-12-27 16:00:23 -0500

    helpppp

--- javascript/slides.js
+++ javascript/slides.js
@@ -1,24 +1,3 @@
-var slideIndex = 1;
-
-var myTimer;
-
-var slideshowContainer;
-
-window.addEventListener("load",function() {
-    showSlides(slideIndex);
-    myTimer = setInterval(function(){plusSlides(1)}, 4000);
-  
-    //COMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
-    slideshowContainer = document.getElementsByClassName('slideshow-inner')[0];
-  
-    //UNCOMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
-    // slideshowContainer = document.getElementsByClassName('slideshow-container')[0];
-  
-    slideshowContainer.addEventListener('mouseenter', pause)
-    slideshowContainer.addEventListener('mouseleave', resume)
-})
-
-// NEXT AND PREVIOUS CONTROL
 function plusSlides(n){
   clearInterval(myTimer);
   if (n < 0){
@@ -26,21 +5,11 @@ function plusSlides(n){
   } else {
    showSlides(slideIndex += 1); 
   }
-  
-  //COMMENT OUT THE LINES BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
-  
   if (n === -1){
     myTimer = setInterval(function(){plusSlides(n + 2)}, 4000);
   } else {
     myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
   }
-}
-
-//Controls the current slide and resets interval if needed
-function currentSlide(n){
-  clearInterval(myTimer);
-  myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
-  showSlides(slideIndex = n);
 }
 
 function showSlides(n){
@@ -59,11 +28,13 @@ function showSlides(n){
   dots[slideIndex-1].className += " active";
 }
 
-pause = () => {
-  clearInterval(myTimer);
-}
+window.addEventListener("load",function() {
+    showSlides(slideIndex);
+    myTimer = setInterval(function(){plusSlides(1)}, 4000);
+})
 
-resume = () =>{
+function currentSlide(n){
   clearInterval(myTimer);
-  myTimer = setInterval(function(){plusSlides(slideIndex)}, 4000);
+  myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
+  showSlides(slideIndex = n);
 }
